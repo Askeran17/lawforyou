aeb3f2725f67704fdb1682286ec2cd8340f8dc4e
Mikhail Atayev <m.ataev@mail.ru>
2024-07-23 13:42:40 +0000

    add urls to bag app

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from services.models import Product
 
 # Create your views here.
+
+def view_bag(request):
+    """ A view that renders the bag contents page """
+
+    return render(request, 'bag/bag.html')
