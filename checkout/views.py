0da9cf72813e80235061386beb8ac46845e84c90
Mikhail Atayev <m.ataev@mail.ru>
2024-07-26 19:53:36 +0000

    added checkout templates css

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.conf import settings
 
-# Create your views here.
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+
+from services.models import Product
