7fe2834b30ebc1e7aa042ed442c897d09322d277
Mikhail Atayev <m.ataev@mail.ru>
2024-07-30 19:38:10 +0000

    fixed profiles views, temolates, static, js, css, etc

--- services/views.py
+++ services/views.py
@@ -24,6 +24,31 @@ def product_detail(request, slug):
     return render(request, 'services/product_detail.html', context)
 
 @login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.url]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'services/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+@login_required
 def edit_product(request, slug):
     """ Edit a product in the store """
     if not request.user.is_superuser:
