bb557c2dbded04ffaec93f9844438648351e7e92
Mikhail Atayev <m.ataev@mail.ru>
2024-08-01 14:08:07 +0000

    edit add function for product and templates for allauth

--- services/urls.py
+++ services/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", ServiceTemplateView.as_view(), name="services"),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('edit/<slug:slug>/', views.edit_product, name='edit_product'),
     path('delete/<slug:slug>/', views.delete_product, name='delete_product'),
 ]
