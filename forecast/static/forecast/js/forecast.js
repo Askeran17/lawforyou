d98e565c5ebcf9049c4124d6194b2297cd2ebe8d
Mikhail Atayev <m.ataev@mail.ru>
2024-08-24 22:47:05 +0000

    added js for forecast and correct css

---
+++ forecast/static/forecast/js/forecast.js
@@ -0,0 +1,217 @@
+/*jshint esversion: 6 */
+/*jshint sub:true*/
+
+// I set this option so JSHint doesn't raise unnecessary warnings.”
+
+//questions count
+let forecastquestionTotal = document.querySelector("#questions-total");
+
+//start screen
+let startPage = document.querySelector('.start-display');
+let startButton = document.getElementById('start-button');
+let forecastContainer = document.getElementById('forecast');
+
+//when user click on start button
+startButton.addEventListener('click', () => {
+	startPage.classList.add('hide');
+	forecastContainer.classList.remove('hide');
+  });
+
+  forecastContainer.classList.add('forecast');
+
+  // Questions and Options array
+const questions = [
+	{
+		question: "Did you send the documents electronically?",
+		answers: ["Yes", "No"],
+		correct: 1,
+	},
+	{
+		question: "Have you been involved with the police in the last 18 months?",
+		answers: [
+			"Yes",
+			"No"
+		],
+		correct: 2,
+	},
+	{
+		question: "Do you have a permanent job?",
+		answers: [
+			"Yes",
+			"No"
+		],
+		correct: 1,
+	},
+	{
+		question: "Has your case been pending for more than 6 months?",
+		answers: ["Yes", "No"],
+		correct: 1,
+	},
+
+    {
+		question: "Have you received any letters about your case?",
+		answers: ["Yes", "No"],
+		correct: 2,
+	},
+
+];
+
+// search elements
+const headerBlock = document.querySelector('#header');
+const listBlock = document.querySelector('#list');
+const submitButton = document.querySelector('#submit');
+
+
+// scores variables
+let correctAnswer = 0; // number of correct answers
+let wrongAnswer = 0; // number of wrong answers
+let score = 0; // games scores
+let questionIndex = 0; // current question
+let questionTotal = 1; // count question
+
+// start commands
+clearPage();
+showQuestion();
+submitButton.onclick = checkAnswer;
+
+// clear page HTML
+function clearPage() {
+    headerBlock.innerHTML = '';
+    listBlock.innerHTML = '';
+
+}
+
+// start current question
+function showQuestion(){
+console.log('showQuestion');
+
+// question
+const headerTemplate = `<h2 class="title">%title%</h2>`;
+const title = headerTemplate.replace('%title%', questions[questionIndex]['question']);
+
+
+headerBlock.innerHTML = title;
+
+// answer options
+let answerNumber = 1;
+for (let answerText of questions[questionIndex]['answers']) {  
+const questionTemplate = 
+     `<li>
+         <label>
+    <input value="%number%" type="radio" class="answer" name="answer" />
+    <span>%answer%</span>
+</label>
+</li>`;
+
+const answerHTML = questionTemplate
+.replace('%answer%', answerText)
+.replace('%number%', answerNumber); 
+
+listBlock.innerHTML += answerHTML;
+answerNumber++;
+
+}
+
+}
+
+// the function will be triggered when the button is pressed
+function checkAnswer(){
+    console.log('checkAnswer started!');
+    
+    // find the selected radio button
+    const checkedRadio = listBlock.querySelector('input[type="radio"]:checked');
+    
+    // if the answer is not selected - do nothing, exit the function
+    if (checkedRadio) {
+        console.log('ok');
+    } else {
+        submitButton.blur();
+        return;
+    }
+    
+    // find out the user's answer number
+    let userAnswer = parseInt(checkedRadio.value); 
+    
+    // if the answer is correct - increase the score. If not alert and continue count questions
+    if (userAnswer === questions[questionIndex]['correct'] ) {
+        correctAnswer++;
+        document.innerHTML=+correctAnswer;
+        document.innerHTML=+score;
+    } else {
+        wrongAnswer++;
+        document.innerHTML=+wrongAnswer;
+        
+           
+    }
+
+    // question lenght
+{ forecastquestionTotal.innerHTML = `<p>${questionTotal + 1} of ${questions.length} questions</p>`;
+questionTotal++;
+	}
+    
+    
+    // checking questions
+    if (questionIndex !== questions.length - 1){
+        console.log('This is NOT last question');
+        questionIndex++;
+        clearPage();
+        showQuestion();
+        return;
+    
+    } else {
+        console.log('This is last question');
+        clearPage();
+        showResult();
+    }
+    
+    }
+
+    // function for result
+function showResult () {
+    console.log(forecastquestionTotal);
+    forecastquestionTotal.classList.add("questions-hidden");
+    console.log('showResult started!');
+    console.log(correctAnswer);
+
+    const resultsTemplate = `
+    <h2 class="title">%title%</h2>
+    <h3 class="summary">%message%</h3>
+    <p class="result">%result%</p>
+    `;
+
+    let title, message;
+
+    // outcome options at the end of the forecast
+    if (correctAnswer === questions.length) {
+        title = 'Good work😀';
+        message = 'The probability of a positive decision is high!';
+
+    } else if ((correctAnswer * 100) / questions.length >= 50) {
+        title = '50/50🤔';
+        message = 'The probability of a positive decision is average';
+
+    } else {
+        title = 'Please complete your case😕';
+        message = 'The probability of a positive decision is low';
+    }
+
+// result
+let result = `${correctAnswer} of ${questions.length}`;
+
+// final answer
+const finalMessage = resultsTemplate
+            .replace('%title%', title)
+            .replace('%message%', message)
+            .replace('%result%', result);
+
+headerBlock.innerHTML = finalMessage;
+
+// play again
+submitButton.blur();
+submitButton.innerText = 'Try again';
+submitButton.onclick = function(){
+    history.go();
+	
+};
+
+}
--- forecast/urls.py
+++ forecast/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("forecast/", views.forecast, name='forecast'),
+    path("", views.forecast, name='forecast'),
 ]
