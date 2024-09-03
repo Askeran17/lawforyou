df67a03298fccea4591e59885ffde093ddb8b08c
Mikhail Atayev <m.ataev@mail.ru>
2024-09-03 13:58:13 +0000

    added featured function on homepage

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from .views import HomeTemplateView
 
 urlpatterns = [
     path("", HomeTemplateView.as_view(), name="home"),
+    path('<slug:slug>/', views.featured, name='featured'),
     path("about/", views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views import generic
 from django.http import HttpResponse
-from django.views.generic.base import TemplateView
+from services.models import Product
 # Create your views here.
 
-class HomeTemplateView(TemplateView):
-    template_name = "home/index.html"
+class HomeTemplateView(generic.ListView):
+    template_name = 'home/index.html'
+    queryset = Product.objects.all()
+    
+def featured(request, slug):
+    queryset = Product.objects.all()
+    product = get_object_or_404(queryset, url=slug)
+
+    return redirect(reverse('product_detail', args=[product.url]))
 
 def about(request):
     return render(request, 'home/about.html')
