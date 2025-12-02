d90132476c96e5d4205c9ffa1fe333dbadf789b2
Askeran17 <m.ataev@mail.ru>
2025-12-02 21:00:30 +0100

    Revert email settings to TLS port 587

--- lawforyou/settings.py
+++ lawforyou/settings.py
@@ -216,9 +216,8 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'lawforyou36@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = False
-    EMAIL_USE_SSL = True
-    EMAIL_PORT = 465
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
